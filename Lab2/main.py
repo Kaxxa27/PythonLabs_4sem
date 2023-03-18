7cbf4bc9b446b409c3f9117dbacf57e949afe946
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-18 22:04:47 +0300

    Первая котрольная точка. Считывание текста из файла и его обработка.

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import fileservice
+import regexservice
+import constants
+#regex = r"[A-Z][^.!?]*((\.{3})|(\.)|(\!?\??\!?))"
+regex = r"\S[^.?!]+[.!?]*"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MY_CONST = constants.CONSTANTS
+# Для чтения и записи. Создаст новый файл для записи, если не найдет с указанным именем.
+# file = open("text.txt", "r")
+# with file as file:
+#     content = file.read()
+#     my_list = re.findall(regex, content)
+
+my_list = regexservice.text_analysis(regex, fileservice.open_file("text.txt", "r"))
+
+i = 0
+new_list = []
+while i < len(my_list):
+    my_str = str(my_list[i])
+    for const in MY_CONST:
+        if my_str.__contains__(const):
+            if i == (len(my_list) - 1):
+                break
+            my_str = my_str + my_list[i+1]
+            i += 1
+
+    new_list.append(my_str)
+    i += 1
+
+# for item in my_list:
+#     print(item)
+for item in new_list:
+    print(item)
+print(len(new_list))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#if __name__ == '__main__':
+    #print(sent for sent in re.findall(regex, string))
