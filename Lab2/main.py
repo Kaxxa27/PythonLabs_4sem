8ba1ce2d05675916fc88b749b4001e607a8f48ff
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-19 16:01:41 +0300

    Task1 finished.

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,38 +1,32 @@
 import fileservice
 import regexservice
 import constants
-#regex = r"[A-Z][^.!?]*((\.{3})|(\.)|(\!?\??\!?))"
-regex = r"\S[^.?!]+[.!?]*"
-
-MY_CONST = constants.CONSTANTS
-# Для чтения и записи. Создаст новый файл для записи, если не найдет с указанным именем.
-# file = open("text.txt", "r")
-# with file as file:
-#     content = file.read()
-#     my_list = re.findall(regex, content)
-
-my_list = regexservice.text_analysis(regex, fileservice.open_file("text.txt", "r"))
-
-i = 0
-new_list = []
-while i < len(my_list):
-    my_str = str(my_list[i])
-    for const in MY_CONST:
-        if my_str.__contains__(const):
-            if i == (len(my_list) - 1):
-                break
-            my_str = my_str + my_list[i+1]
-            i += 1
-
-    new_list.append(my_str)
-    i += 1
-
-# for item in my_list:
-#     print(item)
-for item in new_list:
-    print(item)
-print(len(new_list))
+import stringgluer
+import analizator
 
 
-#if __name__ == '__main__':
-    #print(sent for sent in re.findall(regex, string))
+def main():
+    # regex = r"[A-Z][^.!?]*((\.{3})|(\.)|(\!?\??\!?))"
+    regex = r"\S[^.?!]+[.!?]*"
+
+    text = fileservice.open_file("text.txt", "r")
+    my_list = regexservice.text_analysis_with_regex(regex, text)
+
+    new_list = stringgluer.text_gluer_with_constants(my_list,
+                                                     constants.CONSTANTS_FOR_SWITCHING,
+                                                     constants.CONSTANTS_FOR_CONTINUE)
+
+    am_sent, am_non_sent, av_len_w, av_len_s = analizator.text_analysis(new_list)
+    top_ngram = analizator.generate_ngrams(text, 3)
+
+    print(f"Amount of sentences in the text: {am_sent}")
+    print(f"Amount of non-declarative sentences in the text: {am_non_sent}")
+    print(f"Average length of the sentence in characters: {av_len_s} char(s)")
+    print(f"Average length of the word in the text in characters: {av_len_w} char(s)")
+    print(f"Top-K (Max length) repeated N-grams in the text : ")
+    for index in range(len(top_ngram)):
+        print(f"{index + 1}: {top_ngram[index]}")
+
+
+if __name__ == '__main__':
+    main()
