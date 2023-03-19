e2290837391c71aa3c0e24d21b56bef688c57139
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-19 12:44:14 +0300

    File constants updated(Switch and continue const added)

--- Lab2/constants.py
+++ Lab2/constants.py
@@ -1,7 +1,8 @@
-CONSTANTS = ["Mr.", "Mrs", "etc.", "e.g.", "i.e.", "vs.", "Jr.",
-             "Sr.", "Smth.", "Smb."]
+CONSTANTS_FOR_SWITCHING = ["Re.", "Rf.", "etc.", "e.g.",
+             "V.I.P.", "Smth.", "Smb.", "Edu.", "Appx."]
 
-
+CONSTANTS_FOR_CONTINUE = ["Mr.", "Mrs.", "Miss.", "Ms.", "Dr.", "vs.", "Jr.", "etc.",
+             "Sr.", "Smth.", "Smb.", "e.g.", "V.I.P.", "i.e.", "Re.", "Rf.", "Edu.", "Appx."]
 
 
 
