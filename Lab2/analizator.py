97ee5e48fd1e0bb974bd758f87ac7417c6a8d215
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-19 16:01:05 +0300

    def generate_ngrams added in analizator.py

--- Lab2/analizator.py
+++ Lab2/analizator.py
@@ -1,7 +1,6 @@
 import re
 
 
-# top-K repeated N-grams in the text (K and N are taken from input if needed; by default K=10, N=4).
 def text_analysis(text_list):
     # Amount of sentences in the text.
     amount_of_sentences = len(text_list)
@@ -24,13 +23,8 @@ def text_analysis(text_list):
     average_len_of_the_word /= len(words_list)
     average_len_of_the_sent /= len(text_list)
 
-    print(amount_of_sentences)
-    print(amount_non_declarative_sentences)
-    print(average_len_of_the_word)
-    print(average_len_of_the_sent)
-    # for item in words_list:
-    #     print(item)
-    average_len_of_sentences = 0
+    return amount_of_sentences, amount_non_declarative_sentences, \
+        average_len_of_the_word, average_len_of_the_sent
 
 
 def get_word_list(regex, text_list):
@@ -39,3 +33,15 @@ def get_word_list(regex, text_list):
         words_list += re.findall(regex, item)
     words_list = list(filter(None, words_list))
     return words_list
+
+
+# Top-K repeated N-grams in the text (K and N are taken from input if needed; by default K=10, N=4).
+def generate_ngrams(text, k=10, n=4):
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    words = [word for word in text.split(" ") if word != ""]
+    # Zip(*arr) == zip(arr[0], arr[1], ...)
+    ngrams = zip(*[words[i:] for i in range(n)])
+    max_len_ngrams = [" ".join(ngram) for ngram in ngrams]
+    max_len_ngrams.sort(key=len, reverse=True)
+    return max_len_ngrams[:k]
+
