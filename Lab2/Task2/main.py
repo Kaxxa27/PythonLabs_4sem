b2166fc1a17ff0cccc57b75b4e567c7c4da085ba
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-21 20:44:05 +0300

    main.py updated.

--- Lab2/Task2/main.py
+++ Lab2/Task2/main.py
@@ -1,18 +1,5 @@
 from ConsoleService.console import Console
-from StorageService.storage import Storage
-
-def main():
-    Main_Console = Console()
-    Main_Storage = Storage()
-
-    for i in range(3):
-        Main_Storage.add(input(f"{i + 1}. Add in storage: "))
-
-    Main_Storage.print()
-
-
-    #Main_Console.start()
-
 
 if __name__ == '__main__':
-    main()
+    Main_Console = Console()
+    Main_Console.start()
