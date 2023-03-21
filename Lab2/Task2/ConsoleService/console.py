fad680a7f5edd660fb32a374f6b1f713b0c875e9
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-21 20:44:32 +0300

    Class console updated.

--- Lab2/Task2/ConsoleService/console.py
+++ Lab2/Task2/ConsoleService/console.py
@@ -1,17 +1,62 @@
+from Task2.UserService.user import User
+
+
 class Console:
     def __init__(self):
         pass
 
     def start(self):
+        authorization = False
+        self.print_command()
+
         while True:
-            print("Menu:")
-            print("1. print:")
-            print("2. exit:")
             command = input("Enter command: ")
-            if command == "print":
-                print(self.print_str())
+            if command == "login":
+                authorization = True
+                login = input("Enter your login: ")
+                user = User(login)
+
+                while True:
+                    answer = input("Do you want to load a container? [Yes/No]: ")
+                    if answer == "Yes":
+                        user.load_storage()
+                        break
+                    elif answer == "No":
+                        break
+                    else:
+                        print("Wrong input!\nEnter Yes/No please.")
+            if command == "help":
+                self.print_command()
+                continue
             if command == "exit":
                 break
+            if authorization:
+                if command == "add":
+                    user.add_element(input("(add) Enter element: "))
+                if command == "remove":
+                    user.remove_element(input("(remove) Enter element: "))
+                if command == "list":
+                    user.print_element()
+                if command == "find":
+                    user.find_element(input("(find) Enter element: "))
+                if command == "grep":
+                    user.find_with_regex(input("(grep) Enter regex: "))
+                if command == "save":
+                    user.save_storage()
+                if command == "load":
+                    user.load_storage()
+            else:
+                print("You are not registered.\nPlease register by entering the login command.")
 
-    def print_str(self):
-        return input("Enter string: ")
+    def print_command(self):
+        print("Menu:")
+        print("1. login:")
+        print("2. add: ")
+        print("3. remove: ")
+        print("4. find: ")
+        print("5. grep: ")
+        print("6. save: ")
+        print("7. load: ")
+        print("8. help: ")
+        print("9. list: ")
+        print("10. exit: ")
