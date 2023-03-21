1954662b9eb34cd90b6f21fe481be756a99babdf
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-21 21:57:34 +0300

    COMMANDS added in constants.

--- Lab2/Task2/Constants/constants.py
+++ Lab2/Task2/Constants/constants.py
@@ -1,3 +1,18 @@
 from pathlib import Path
 
 STORAGE_PATH = Path("/home/eugene/PythonLabs/PythonLabs_4sem/Lab2/Task2/StorageFiles/")
+
+COMMANDS = [
+    "login",
+    "add",
+    "remove",
+    "find",
+    "grep",
+    "save",
+    "load",
+    "help",
+    "list",
+    "clear",
+    "exit",
+]
+
