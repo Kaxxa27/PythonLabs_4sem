332b5f342df379dd5eb8c21f8442e8f565747ca2
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-22 22:26:40 +0300

    Task2/main.py updated.

--- Lab2/Task2/main.py
+++ Lab2/Task2/main.py
@@ -1,4 +1,8 @@
-from ConsoleService.console import Console
+import sys
+
+sys.path.append("..")
+
+from Task2.ConsoleService.console import Console
 
 if __name__ == '__main__':
     Main_Console = Console()
