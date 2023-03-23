2e6af540e46e0e754abb5ddfbf1163b60df88811
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-03-23 09:20:43 +0300

    Updated constant Task1

--- Lab2/Task1/constants.py
+++ Lab2/Task1/constants.py
@@ -1,8 +1,8 @@
-CONSTANTS_FOR_SWITCHING = ["Re.", "Rf.", "etc.", "e.g.",
-             "V.I.P.", "Smth.", "Smb.", "Edu.", "Appx."]
+CONSTANTS_FOR_SWITCHING = ["Re.", "Rf.", "etc.",
+              "Smth.", "Smb.", "Edu.", "Appx."]
 
 CONSTANTS_FOR_CONTINUE = ["Mr.", "Mrs.", "Miss.", "Ms.", "Dr.", "vs.", "Jr.", "etc.",
-             "Sr.", "Smth.", "Smb.", "e.g.", "V.I.P.", "i.e.", "Re.", "Rf.", "Edu.", "Appx."]
+             "Sr.", "Smth.", "Smb.", "Re.", "Rf.", "Edu.", "Appx."]
 
 
 
