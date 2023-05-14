4d21e43ec9cbb6a85f188d2cb522b3fc89d3143c
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-05-14 13:06:49 +0300

    Added private method in Serializer.

--- Lab3/serializer/serializer.py
+++ Lab3/serializer/serializer.py
@@ -15,82 +15,82 @@ class Serializer:
     def serialize(self, obj):
         # Serialization of basic types.
         if isinstance(obj, tuple(BASIC_TYPES.values())):
-            return self.serialize_basic_types(obj)
+            return self.__serialize_basic_types__(obj)
 
         # Serialization None type.
         elif isinstance(obj, types.NoneType):
-            return self.serialize_none_type()
+            return self.__serialize_none_type__()
 
         # Serialization of same sequence types.
         # [ list, tuple, frozenset, set, bytes, bytearray ].
         elif isinstance(obj, tuple(SAME_SEQUENCE_TYPES.values())):
-            return self.serialize_same_sequence_types(obj)
+            return self.__serialize_same_sequence_types__(obj)
 
         # Serialization of mapping types.
         # [ dict ]
         elif isinstance(obj, tuple(MAPPING_TYPES.values())):
-            return self.serialize_mapping_types(obj)
+            return self.__serialize_mapping_types__(obj)
 
         # Serialization function.
         elif inspect.isfunction(obj):
-            return self.serialize_function(obj)
+            return self.__serialize_function__(obj)
 
         # Serialization code.
         elif inspect.iscode(obj):
-            return self.serialize_code(obj)
+            return self.__serialize_code__(obj)
 
         # Serialization cell.
         elif isinstance(obj, types.CellType):
-            return self.serialize_cell(obj)
+            return self.__serialize_cell__(obj)
 
         # Serialization class.
         elif inspect.isclass(obj):
-            return self.serialize_class(obj)
+            return self.__serialize_class__(obj)
 
         else:
-            return self.serialize_object(obj)
+            return self.__serialize_object__(obj)
 
-    def serialize_basic_types(self, obj):
+    def __serialize_basic_types__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = self.get_object_type(obj)
         serialize_result["value"] = obj
         return serialize_result
 
-    def serialize_none_type(self):
+    def __serialize_none_type__(self):
         serialize_result = dict()
         serialize_result["type"] = "NoneType"
         # serialize_result["value"] = "definitely none"
         serialize_result["value"] = "None"
         return serialize_result
 
-    def serialize_same_sequence_types(self, obj):
+    def __serialize_same_sequence_types__(self, obj):
         serialize_result = dict()
-        serialize_result["type"] = self.get_obj_type(obj)
+        serialize_result["type"] = Serializer.get_object_type(obj)
         serialize_result["value"] = [self.serialize(item) for item in obj]
         return serialize_result
 
-    def serialize_mapping_types(self, obj):
+    def __serialize_mapping_types__(self, obj):
         serialize_result = dict()
-        serialize_result["type"] = self.get_obj_type(obj)
+        serialize_result["type"] = Serializer.get_object_type(obj)
         serialize_result["value"] = [[self.serialize(key), self.serialize(value)] for (key, value) in obj.items()]
         return serialize_result
 
-    def serialize_function(self, obj):
+    def __serialize_function__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = "function"
-        serialize_result["value"] = self.total_func_serialize(obj)
+        serialize_result["value"] = self.__total_func_serialize__(obj)
         return serialize_result
 
-    def total_func_serialize(self, obj, cls=None):
+    def __total_func_serialize__(self, obj, cls=None):
         value = dict()
         value["__name__"] = obj.__name__
-        value["__globals__"] = self.get_globals(obj, cls)
+        value["__globals__"] = self.__get_globals__(obj, cls)
         value["__closure__"] = self.serialize(obj.__closure__)
         value["__code__"] = {key: self.serialize(value) for key, value in inspect.getmembers(obj.__code__)
                              if key in CODE_PROPERTIES}
         return value
 
-    def get_globals(self, obj, cls=None):
+    def __get_globals__(self, obj, cls=None):
         """Work with __globals__ and __code__"""
 
         globs = dict()
@@ -118,26 +118,26 @@ class Serializer:
 
         return globs
 
-    def serialize_code(self, obj):
+    def __serialize_code__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = "code"
         serialize_result["value"] = {key: self.serialize(value) for key, value in inspect.getmembers(obj)
                                      if key in CODE_PROPERTIES}
         return serialize_result
 
-    def serialize_cell(self, obj):
+    def __serialize_cell__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = "cell"
         serialize_result["value"] = self.serialize(obj.cell_contents)
         return serialize_result
 
-    def serialize_class(self, obj):
+    def __serialize_class__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = "class"
-        serialize_result["value"] = self.total_class_serialize(obj)
+        serialize_result["value"] = self.__total_class_serialize__(obj)
         return serialize_result
 
-    def total_class_serialize(self, obj):
+    def __total_class_serialize__(self, obj):
         serialize_result = dict()
         serialize_result["__name__"] = self.serialize(obj.__name__)
 
@@ -150,23 +150,23 @@ class Serializer:
                 serialize_result[key] = dict()
                 serialize_result[key]["type"] = "staticmethod"
                 serialize_result[key]["value"] = {
-                    "type": "function", "value": self.total_function_serialize(value.__func__, obj)
+                    "type": "function", "value": self.__total_func_serialize__(value.__func__, obj)
                 }
 
             elif isinstance(obj.__dict__[key], classmethod):
                 serialize_result[key] = dict()
                 serialize_result[key]["type"] = "classmethod"
                 serialize_result[key]["value"] = {
-                    "type": "function", "value": self.total_function_serialize(value.__func__, obj)
+                    "type": "function", "value": self.__total_func_serialize__(value.__func__, obj)
                 }
 
             elif inspect.ismethod(value):
-                serialize_result[key] = self.total_function_serialize(value.__func__, obj)
+                serialize_result[key] = self.__total_func_serialize__(value.__func__, obj)
 
             elif inspect.isfunction(value):
                 serialize_result[key] = dict()
                 serialize_result[key]["type"] = "function"
-                serialize_result[key]["value"] = self.total_function_serialize(value, obj)
+                serialize_result[key]["value"] = self.__total_func_serialize__(value, obj)
 
             else:
                 serialize_result[key] = self.serialize(value)
@@ -177,13 +177,13 @@ class Serializer:
 
         return serialize_result
 
-    def serialize_object(self, obj):
+    def __serialize_object__(self, obj):
         serialize_result = dict()
         serialize_result["type"] = "object"
-        serialize_result["value"] = self.total_object_serialization(obj)
+        serialize_result["value"] = self.__total_object_serialization__(obj)
         return serialize_result
 
-    def total_object_serialization(self, obj):
+    def __total_object_serialization__(self, obj):
         value = dict()
         value["__class__"] = self.serialize(obj.__class__)
         value["__members__"] = {key: self.serialize(value) for key, value in inspect.getmembers(obj)
@@ -193,33 +193,33 @@ class Serializer:
     def deserialize(self, obj):
 
         if obj["type"] in self.extract_keys(str(BASIC_TYPES.keys())):
-            return self.deserialize_basic_types(obj)
+            return self.__deserialize_basic_types__(obj)
 
         elif obj["type"] in str(SAME_SEQUENCE_TYPES.keys()):
-            return self.deserialize_collections(obj)
+            return self.__deserialize_collections__(obj)
 
         elif obj["type"] == "code":
-            return self.deserialize_code(obj["value"])
+            return self.__deserialize_code__(obj["value"])
 
         elif obj["type"] == "function":
-            return self.deserialize_function(obj["value"])
+            return self.__deserialize_function__(obj["value"])
 
         elif obj["type"] == "cell":
-            return self.deserialize_cell(obj)
+            return self.__deserialize_cell__(obj)
 
         elif obj["type"] == "class":
-            return self.deserialize_class(obj["value"])
+            return self.__deserialize_class__(obj["value"])
 
         elif obj["type"] in DECORATOR_METHODS:
             return DECORATOR_METHODS[obj["type"]](self.deserialize(obj["value"]))
 
         elif obj["type"] == "object":
-            return self.deserialize_object(obj["value"])
+            return self.__deserialize_object__(obj["value"])
 
-    def deserialize_basic_types(self, obj):
+    def __deserialize_basic_types__(self, obj):
         return BASIC_TYPES[obj["type"]](obj["value"])
 
-    def deserialize_collections(self, obj):
+    def __deserialize_collections__(self, obj):
         collection_type = obj["type"]
 
         if collection_type in SAME_SEQUENCE_TYPES.keys():
@@ -229,21 +229,21 @@ class Serializer:
             return ALL_COLLECTIONS_TYPES[collection_type](
                 {self.deserialize(item[0]): self.deserialize(item[1]) for item in obj["value"]})
 
-    def deserialize_code(self, code):
+    def __deserialize_code__(self, code):
         return types.CodeType(*(self.deserialize(code[prop]) for prop in CODE_PROPERTIES))
 
-    def deserialize_function(self, func):
+    def __deserialize_function__(self, func):
         code = func["__code__"]
         globs = func["__globals__"]
         func_closure = func["__closure__"]
-        des_globals = self.deserialize_globals(globs, func)
+        des_globals = self.__deserialize_globals__(globs, func)
 
         cl = self.deserialize(func_closure)
         if cl:
             closure = tuple(cl)
         else:
             closure = tuple()
-        codeType = self.deserialize_code(code)
+        codeType = self.__deserialize_code__(code)
 
         des_globals["__builtins__"] = __import__("builtins")
         des_function = types.FunctionType(code=codeType, globals=des_globals, closure=closure)
@@ -251,7 +251,7 @@ class Serializer:
 
         return des_function
 
-    def deserialize_globals(self, globs, func):
+    def __deserialize_globals__(self, globs, func):
         des_globals = dict()
 
         for glob in globs:
@@ -263,10 +263,10 @@ class Serializer:
 
         return des_globals
 
-    def deserialize_cell(self, obj):
+    def __deserialize_cell__(self, obj):
         return types.CellType(self.deserialize(obj["value"]))
 
-    def deserialize_class(self, obj):
+    def __deserialize_class__(self, obj):
         bases = self.deserialize(obj["__bases__"])
 
         members = {member: self.deserialize(value) for member, value in obj.items()}
@@ -281,12 +281,13 @@ class Serializer:
 
         return cls
 
-    def deserialize_object(self, obj):
+    def __deserialize_object__(self, obj):
         cls = self.deserialize(obj["__class__"])
         des = object.__new__(cls)
         des.__dict__ = {key: self.deserialize(value) for key, value in obj["__members__"].items()}
         return des
 
     # def(BASIC_TYPES) -> return  str: [ int, float, complex, ... ]
-    def extract_keys(self, string) -> str:
+    @staticmethod
+    def extract_keys(string) -> str:
         return re.search(r"\[.*\]", string).group()
