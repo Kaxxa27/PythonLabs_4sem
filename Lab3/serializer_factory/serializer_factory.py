b5e720df4a8946bc35fe682d5b5f6afbd3df6b41
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-05-26 15:00:34 +0300

    xml_ser added to serializer_factory.

--- Lab3/serializer_factory/serializer_factory.py
+++ Lab3/serializer_factory/serializer_factory.py
@@ -1,6 +1,6 @@
 from serializer.serializer import Serializer
 from json_serializer.json_serializer import JSON_Serializer
-
+from xml_serializer.xml_serializer import XML_Serializer
 
 class Serializer_Factory:
     """This factory for creating serializer {json/xml}"""
@@ -10,12 +10,14 @@ class Serializer_Factory:
         Return json_serializer if str: json
         Return xml_serializer if str: xml
         """
-        serializer_frmt = serializer_frmt.lower()
+        serializer_frmt = serializer_frmt.strip().lower()
 
         if serializer_frmt == "json":
             return JSON_Serializer()
-        # elif serializer_frmt == "xml":
-        #     return
+        elif serializer_frmt == "xml":
+            return XML_Serializer()
+        else:
+            raise NameError("From create_serializer: Invalid serializer name [json, xml].")
 
 
 
