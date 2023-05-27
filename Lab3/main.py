270df7c3d0f4980eeea56f2fc6b668096f7b8af3
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-05-27 13:53:13 +0300

    small changes main

--- Lab3/main.py
+++ Lab3/main.py
@@ -38,23 +38,68 @@ def test(sep=" "):
 
 
 if __name__ == '__main__':
-    json_ser = Serializer_Factory.create_serializer("json")
-    json = JSON_Serializer()
-    data = test
+    factory = Serializer_Factory()
+    json_ser = factory.create_serializer("json")
+    xml_ser = factory.create_serializer("xml")
+    data = Human(12,"Jeka")
     file = "Data.txt"
 
-    # pprint(data("Jeka HOLLLA            "))
     with open(file, 'w') as f:
-        json.dump(data, f)
+        xml_ser.dump(data, f)
 
     with open(file, 'r') as f:
-        result = json.load(f)
+        result = xml_ser.load(f)
 
     pprint(open(file, "r").read())
     pprint(type(result))
+    # pprint(list(result))
 
-    test = result()
-    pprint(test("Jeak sdfs f      "))
+    # test = result()
+    # pprint(test("Jeak sdfs f      "))
 
 
 
+
+# import math
+# from pprint import pprint
+#
+# from json_serializer.json_serializer import JSON_Serializer
+#
+#
+# class A:
+#     def my_method(self):
+#         return 5
+#
+#
+# class B:
+#     def another_method(self):
+#         return 6
+#
+#
+# class C(A, B):
+#     pass
+#
+#
+# x = 10
+#
+#
+# def my_func(a):
+#     return math.sin(x * a)
+#
+#
+# json_ser = JSON_Serializer()
+# obj = C()
+# ser_obj = json_ser.dumps(obj)
+# deser_obj = json_ser.loads(ser_obj)
+# pprint(deser_obj.my_method())  # returns 5
+# pprint(deser_obj.another_method())  # returns 6
+#
+# ser_class = json_ser.dumps(C)
+# deser_class = json_ser.loads(ser_class)
+# obj = deser_class()
+# pprint(obj.my_method())   # returns 5
+# pprint(obj.another_method())   # returns 6
+#
+# ser_func = json_ser.dumps(my_func)
+# deser_func = json_ser.loads(ser_func)
+# pprint(deser_func(20))   # returns sin(10 * 20)
