a8b9bfcca9a5227c1be7d818ac781f757cd071ab
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-06-03 00:34:20 +0300

    parking_list & rent_parking added,

--- Lab4/myparking/urls.py
+++ Lab4/myparking/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path(r'', views.index, name='index'),
      path('register/', views.registration_view, name='register'),
+     path('parking_list/', views.parking_list, name='parking_list'),
+     path('rent_parking/<int:id>/', views.rent_parking, name='rent_parking'),
     # path('cars/', views.car_list, name='car_list'),  # URL-шаблон для списка автомобилей
     # path('cars/<int:pk>/', views.car_detail, name='car_detail'),  # URL-шаблон для деталей автомобиля
 ]
--- Lab4/myparking/views.py
+++ Lab4/myparking/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import RegistrationForm
 from .models import *
@@ -27,3 +29,50 @@ def registration_view(request):
     else:
         form = RegistrationForm()
     return render(request, 'registration.html', {'form': form})
+
+def parking_list(request):
+    filter_busy = request.GET.get('busy')
+    filter_min_price = request.GET.get('min_price')
+    filter_max_price = request.GET.get('max_price')
+
+    parkings = ParkingSpot.objects.all()
+
+    if filter_busy == 'busy':
+        parkings = parkings.filter(is_busy=True)
+    elif filter_busy == 'free':
+        parkings = parkings.filter(is_busy=False)
+
+    if filter_min_price:
+        parkings = parkings.filter(price__gte=float(filter_min_price))
+    if filter_max_price:
+        parkings = parkings.filter(price__lte=float(filter_max_price))
+
+    parkings_count = parkings.count()
+
+    return render(
+        request,
+        'myparking/parking_list.html',
+        context={'parkings': parkings, 'parkings_count': parkings_count, },
+    )
+
+
+def rent_parking(request, id):
+    parking = get_object_or_404(ParkingSpot, id=id)
+    user = request.user
+
+    if request.method == 'POST':
+        # Присвоение парковочного места пользователю
+        user.parkings.add(parking)
+        payment = Payment(owner=user, amount=parking.price, date=datetime.date.today())
+        payment.save()
+        user.payments.add(payment)
+
+        parking.is_busy = True
+        parking.save()
+        return redirect('parking_list')  # Перенаправление на список парковочных мест
+
+    return render(
+        request,
+        'myparking/rent_parking.html',
+        context={'parking': parking, },
+    )
