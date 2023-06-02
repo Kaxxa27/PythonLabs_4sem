312bd80cb6e4a295f6dc9e96ef13ea315dfc00ef
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-06-02 18:40:59 +0300

    models has been registrated in myparking/admin.py.

--- Lab4/myparking/admin.py
+++ Lab4/myparking/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'mark', 'model', 'license_plate', 'parking_spot')
+
+
+@admin.register(ParkingSpot)
+class ParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ('number', 'price', 'is_busy')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('client', 'amount', 'date')
+    list_filter = ('date', 'amount')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('client', 'amount')
+    list_filter = ('amount', 'client')
